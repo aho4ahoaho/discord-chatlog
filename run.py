dccb6e70771113c2461036011ad4c8a581aac06e
aho4ahoaho <aho4ahoaho@outlook.jp>
2020-10-30 04:16:24 +0900

    update

--- run.py
+++ run.py
@@ -27,7 +27,7 @@ async def on_message(message):
         f.write(message.author.name+"@"+message.author.avatar+":"+message.content+"\n")
     
     if message.content.startswith("情報開示請求"):
-        await message.channel.send(content="{0:%Y-%m-%d %H:%M:%S}".format(datetime.datetime.now())+"時点でのチャットログ",file=discord.File(os.getcwd()+"\\"+filepath,filename=message.channel.name+".txt"),delete_after=300)
+        await message.channel.send(content="{0:%Y-%m-%d %H:%M:%S}".format(datetime.datetime.now())+"時点でのチャットログ",file=discord.File(os.getcwd()+"/"+filepath,filename=message.channel.name+".txt"),delete_after=300)
 
 
 client.run(sys.argv[1])
